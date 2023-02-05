b4a62b17c3771431578e99ada949facbdc93d692
sushant0489 <114470086+sushant0489@users.noreply.github.com>
2023-02-05 12:46:18 +0530

    Add files via upload

---
+++ hello.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+int main()
+{
+    std::cout << "Hello World" << std::endl;
+    for (int i = 0; i < 4; i++)
+    {
+        std::cout << i << std::endl;
+    }
+    
+    return 0;
+}
